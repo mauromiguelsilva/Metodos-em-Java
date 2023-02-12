99dd4f95e28a2e47f0fc21e4406a5fe6e0ca76a1
miguel <mauromiguel007@gmail.com>
2023-02-12 12:26:58 -0300

    initial commit

---
+++ Metodos2/src/Metodos2/Main.java
@@ -0,0 +1,16 @@
+package Metodos2;
+
+public class Main {
+	
+	public static void main(String [] args) {
+
+		Qaudrilatero.area(3);
+		Qaudrilatero.area(5,6);
+		Qaudrilatero.area(7, 8, 9);
+		Qaudrilatero.area(5f,5f);
+		
+	}
+
+	
+	
+}
---
+++ Metodos2/src/Metodos2/Qaudrilatero.java
@@ -0,0 +1,31 @@
+package Metodos2;
+
+public class Qaudrilatero {
+	
+	public static void area(double lado) {
+		
+		System.out.println("A area do quadrado é : " + lado * lado);
+		
+	}
+	
+	
+	public static void area(double lado1,double lado2) {
+		
+		System.out.println("A area do triangulo é : " + lado1 * lado2);
+		
+	}
+	
+	
+	
+	public static void area(double baseMaior,double baseMenor, double altura) {
+		
+		 System.out.println("Área do trapézio : " + ((baseMaior+baseMenor)*altura) / 2);
+		
+	}
+
+	
+    public static void area(float diagonal1, float diagonal2) {
+
+        System.out.println("Área do losango:" + (diagonal1 * diagonal2)/2);
+    }
+}
